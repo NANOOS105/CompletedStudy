df908919b99d1b07a462f51bebd9e034a3d84e0c
NANOOS105 <heesu105@naver.com>
2025-01-06 11:37:55 +0900

    application.yml 설정

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,26 +1,20 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.service.user.UserService;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import com.group.libraryapp.service.user.UserServiceV1;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceV1 userServiceV1;
 
-    public UserController(UserService userService){
-        this.userService = userService;
+    public UserController(UserServiceV1 userServiceV1){
+        this.userServiceV1 = userServiceV1;
     }
 
     @PostMapping("/user")
@@ -31,7 +25,7 @@ public class UserController {
 
 //        String sql = "INSERT INTO user(name, age) VALUES(?, ?)";
 //        jdbcTemplate.update(sql, request.getName(), request.getAge());
-        userService.saveUser(request);
+        userServiceV1.saveUser(request);
     }
 
     @GetMapping("/user")
@@ -45,17 +39,17 @@ public class UserController {
 //                int age = rs.getInt("age");
 //                return new UserResponse(id, name, age);
 //            }
-        return userService.getUsers();
+        return userServiceV1.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        userService.updateUser(request);
+        userServiceV1.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-        userService.deleteUser(name);
+        userServiceV1.deleteUser(name);
     }
 
     @GetMapping("/user/error-test")
