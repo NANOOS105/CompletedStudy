62450f24ca939bb46d7a67e081747a12e8443fd9
NANOOS105 <heesu105@naver.com>
2025-01-16 14:33:21 +0900

    module.css 도입

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,10 @@
-const names = ['Maximilian', 'Manuel'];
+import classes from './Post.module.css';
 
-
-function Post(){
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-    return (<div>
-        <p>{chosenName}</p>
-        <p>React.js is awesome!</p>
+function Post(props){
+    return (
+        <div className= {classes.post}>
+        <p className={classes.author}>{props.author}</p>
+        <p className={classes.text}>{props.body}</p>
     </div>
     );
 }
