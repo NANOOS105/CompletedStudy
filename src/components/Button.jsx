05f2a788a80e74b3601a520bec65db9b683e1d5c
NANOOS105 <heesu105@naver.com>
2025-02-01 00:00:04 +0900

    onClickButton, 합성이벤트

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,15 @@
 const Button = ({ text, color = "black" ,children}) => {
+    //이벤트 객체
+    const onClickButton = (e) => {
+        console.log(text);
+        console.log(e);
+    };
+
     return(
-        <button style={{ color: color }}>
+        <button
+            onClick={onClickButton}
+            // onMouseEnter={onClickButton}
+            style={{ color: color }}>
             {text} - {color.toUpperCase()}
             {children}
         </button>
@@ -8,3 +17,5 @@ const Button = ({ text, color = "black" ,children}) => {
 };
 
 export default Button;
+
+//합성 이벤트(통일된 규칙) => 브라우저의 종류에 상관이 없게끔
