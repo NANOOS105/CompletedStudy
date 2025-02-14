d7145d60a911e9c30feebd16d88739b30e843627
NANOOS105 <heesu105@naver.com>
2025-02-14 18:33:08 +0900

    더미데이터 추가

--- src/todolist/pages/Todolist.jsx
+++ src/todolist/pages/Todolist.jsx
@@ -2,8 +2,30 @@ import Header from "../component/Header";
 import Editor from "../component/Editor";
 import List from "../component/List";
 import "./Todolist.css";
+import {useState} from "react";
+const mockData=[
+    {
+        id: 0,
+        isDone : false,
+        content : "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone : false,
+        content : "Java 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone : false,
+        content : "빨래하기",
+        date: new Date().getTime(),
+    },
+];
 
 const Todolist = () => {
+    const [todos,setTodos] = useState(mockData);
     return (
         <div className="Todolist">
             <Header />
