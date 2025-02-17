372ce408dfd9028d79797de6ba4c5a9e553e763a
Heesu <heesu105@naver.com>
2025-02-17 15:43:51 +0900

    검색어 코드 생성

--- src/todolist/component/List.jsx
+++ src/todolist/component/List.jsx
@@ -1,16 +1,45 @@
 import "./List.css";
-import todoItem from "./TodoItem";
+import {useState} from "react";
 import TodoItem from "./TodoItem";
 
 const List = () => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredData = () => {
+        if (search === "") {
+            return todos;
+        }
+        return todos.filter((todo) =>
+            todo.content
+                .toLowerCase()
+                .includes(search.toLowerCase())
+        );
+    };
+
+    const filteredTodos = getFilteredData();
+
     return (
         <div className="List">
-            <h4>To do List🌱</h4>
-            <input placeholder="검색어를 입력하세요"/>
+            <h4>Todo List 🌱</h4>
+            <input
+                value={search}
+                onChange={onChangeSearch}
+                placeholder="검색어를 입력하세요"
+            />
             <div className="todos_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {filteredTodos.map((todo) => {
+                    return (
+                        <TodoItem
+                            key={todo.id}
+                            {...todo}
+                            onUpdate={onUpdate}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
