75a01552b33bc2ac7ae0b48b76575d290d64a5e7
Heesu <heesu105@naver.com>
2025-02-17 17:08:10 +0900

    useReducer 공부

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Count from "./count/pages/Count";
 import Todolist from "./todolist/pages/Todolist";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Exam from "./todolist/component/Exam";
 
 const Home = () => {
     return <h1>홈 페이지입니다!</h1>;
@@ -15,12 +16,14 @@ function App() {
             <Link to="/">홈</Link> |
             <Link to="/counter">Counter</Link>
             <Link to="/todolist">Todolist</Link>
+            <Link to="/exam">Exam</Link>
         </nav>
 
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/counter" element={<Count />} />
             <Route path="/todolist" element={<Todolist />} />
+            <Route path="/exam" element={<Exam />} />
         </Routes>
     </Router>
   );
