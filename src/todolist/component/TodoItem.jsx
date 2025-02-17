e4c129b3c4ec3030325bd547f7dd1cd9c332f187
Heesu <heesu105@naver.com>
2025-02-17 15:48:06 +0900

    변수 추가해서 Item값 채우기

--- src/todolist/component/TodoItem.jsx
+++ src/todolist/component/TodoItem.jsx
@@ -1,13 +1,30 @@
 import "./TodoItem.css";
 
-const TodoItem = () => {
+const TodoItem = ({
+                      id,
+                      isDone,
+                      content,
+                      date,
+                      onUpdate,
+                  }) => {
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    };
+
     return (
-      <div className="TodoItem">
-        <input type="checkbox" />
-          <div className="content">Todo..</div>
-          <div className="date">Date</div>
-          <button>삭제</button>
-      </div>
+        <div className="TodoItem">
+            <input
+                onChange={onChangeCheckbox}
+                readOnly
+                checked={isDone}
+                type="checkbox"
+            />
+            <div className="content">{content}</div>
+            <div className="date">
+                {new Date(date).toLocaleDateString()}
+            </div>
+            <button>삭제</button>
+        </div>
     );
 };
 
