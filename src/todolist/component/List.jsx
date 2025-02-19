1f9a96a2ccce6deea30d43136264938e025e4a89
Heesu <heesu105@naver.com>
2025-02-19 16:59:02 +0900

    getAnalizedData

--- src/todolist/component/List.jsx
+++ src/todolist/component/List.jsx
@@ -22,9 +22,29 @@ const List = ({todos, onUpdate, onDelete}) => {
 
     const filteredTodos = getFilteredData();
 
+    const getAnalizedData = () => {
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo)=>todo.isDone
+        ).length;
+        const notDoneCount = totalCount - doneCount;
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        }
+
+    };
+
+     const {totalCount, doneCount, notDoneCount} =
+         getAnalizedData();
+
     return (
         <div className="List">
             <h4>Todo List 🌱</h4>
+            <div>total: {totalCount}</div>
+            <div>done: {doneCount}</div>
+            <div>notDone: {notDoneCount}</div>
             <input
                 value={search}
                 onChange={onChangeSearch}
