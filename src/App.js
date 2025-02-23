1975c9a0e2179a29a07f8a596119bcf083250598
Heesu <heesu105@naver.com>
2025-02-24 01:56:38 +0900

    mockData 생성

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate} from "react-router-dom";
+import {useReducer} from "react";
+import { Routes, Route,} from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import {getEmotionImage} from "./util/get-emotion-image";
+import Edit from "./pages/Edit";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+    {
+        id:1 ,
+        createDate:new Date().getTime(),
+        emotionId:1 ,
+        content:"1번 일기 내용" ,
+    },
+    {
+        id:2 ,
+        createDate:new Date().getTime(),
+        emotionId:2 ,
+        content:"2번 일기 내용" ,
+    }
+]
+function reducer(state,action){
+    return state;
+}
+
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-     nav("/new");
-  };
-
+  const [data, dispatch] = useReducer(reducer, [mockData]);
   return (
       <>
-          <div>
-              <img src={getEmotionImage(1)} />
-              <img src={getEmotionImage(2)} />
-              <img src={getEmotionImage(3)} />
-              <img src={getEmotionImage(4)} />
-              <img src={getEmotionImage(5)} />
-          </div>
-        <div>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/new"}>New</Link>
-            <Link to={"/diary"}>Diary</Link>
-        </div>
-        <button onClick={onClickButton}>
-              New 페이지로 이동
-        </button>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/diary/:id" element={<Diary />} />
           <Route path="*" element={<Notfound />} />
+          <Route path="/edit/:id" element={<Edit />} />
         </Routes>
       </>
   );
