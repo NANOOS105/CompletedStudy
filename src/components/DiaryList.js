1b94bfeb1fa43374f85ff553e280d552ebd41557
Heesu <heesu105@naver.com>
2025-03-04 17:22:42 +0900

    최신순 오래된 순 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -12,10 +12,22 @@ const DiaryList = ({data}) => {
         setSortType(e.target.value);
     };
 
+    const getSortedData = () => {
+        return data.toSorted((a, b) => {
+            if (sortType === "oldest") {
+                return Number(a.createDate) - Number(b.createDate);
+            } else {
+                return Number(b.createDate) - Number(a.createDate);
+            }
+        });
+    };
+
+    const sortedData = getSortedData();
+
     return (
       <div className="DiaryList">
           <div className="menu_bar">
-              <select onChange      ={onChangeSortType}>
+              <select onChange ={onChangeSortType}>
                   <option value={"latest"}>최신순</option>
                   <option value={"oldest"}>오래된 순</option>
               </select>
@@ -24,7 +36,7 @@ const DiaryList = ({data}) => {
                   text={"새 일기 쓰기"} type={"POSITIVE"}/>
           </div>
           <div className="list_wrapper">
-              {data.map((item)=><DiaryItem key={item.id}{...item}/>)}
+              {sortedData.map((item)=><DiaryItem key={item.id}{...item}/>)}
           </div>
       </div>
     );
